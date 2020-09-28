938002fa225b2ae2edbe7dcdbbe4ee7cb0849e13
MarkVosika <32706691+MarkVosika@users.noreply.github.com>
2020-09-28 17:26:11 -0400

    Update Att&ck_TAXII_2_Excel_Matrix.py
    Updated to support Python3, columns are now in proper left to right order, and now includes sub-techniques.  If a sub-technique is found, the parent technique won't be added.

--- Att&ck_TAXII_2_Excel_Matrix.py
+++ Att&ck_TAXII_2_Excel_Matrix.py
@@ -1,36 +1,41 @@
-#import needed libraries
+# Import needed libraries
 from stix2 import TAXIICollectionSource, Filter
-from taxii2client import Server, Collection
+from taxii2client.v20 import Server, Collection
 import json
+import re
 from openpyxl import Workbook
 from openpyxl.styles import Font
 from openpyxl.styles import Alignment
 
-#enterprise attack source
+#__________________________________________________________________________________________________________________________
+# Enterprise attack source
 collection = Collection("https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/")
-# supply the TAXII2 collection to TAXIICollection
+# Supply the TAXII2 collection to TAXIICollection
 tc_source = TAXIICollectionSource(collection)
 
-#filter to only techniques
+# Filter to only techniques
 filt = Filter('type', '=', 'attack-pattern')
 techniques = tc_source.query([filt])
 
-
-#generate list of all kill chain phases
+#__________________________________________________________________________________________________________________________
+# Generate list of all kill chain phases and put them in proper order
 kc_list = []
 
 for technique in techniques:
 	for k,v in technique.items():
-		for i in technique["kill_chain_phases"]:
-			for k,v in i.items():
-				kc_list.append(v.encode("utf-8"))
+		if k == "kill_chain_phases":
+			for i in v:
+				for k,v in i.items():
+					kc_list.append(v)
 
-#remove duplicates and sort
+# Remove duplicates and sort
 deduped_kc = sorted(list(dict.fromkeys(kc_list)))
 deduped_kc.remove('mitre-attack')
-print deduped_kc
+reorder = [8,5,10,11,3,2,4,9,0,1]
+deduped_kc = [deduped_kc[i] for i in reorder]
 
-#seperate platforms
+#__________________________________________________________________________________________________________________________
+# Seperate platforms
 windows = []
 linux = []
 mac = []
@@ -47,12 +52,12 @@ for technique in techniques:
     		if "macOS" in v:
     			mac.append(technique)
 
-
-#create worksheet tabs
+#__________________________________________________________________________________________________________________________
+# Create worksheet tabs and variable to hold mitre data (list_of_lists)
 count = 1
 
 wb = Workbook()
-sheet1 = wb.get_sheet_by_name('Sheet')
+sheet1 = wb['Sheet']
 sheet1.title = 'All_Techniques'
 sheet2 = wb.create_sheet('Windows')
 sheet3 = wb.create_sheet('Linux')
@@ -60,6 +65,35 @@ sheet4 = wb.create_sheet('macOS')
 
 list_of_lists = []
 
+#__________________________________________________________________________________________________________________________
+# Fuction to create a flatlist of a nested list, for each list in list_of_lists
+def flattenNestedList(nestedList):
+    ''' Converts a nested list to a flat list '''
+    flatList = []
+    # Iterate over all the elements in given list
+    for elem in nestedList:
+        # Check if type of element is list
+        if isinstance(elem, list):
+            # Extend the flat list by adding contents of this element (list)
+            flatList.extend(flattenNestedList(elem))
+        else:
+            # Append the elemengt to the list
+            flatList.append(elem)    
+    return flatList
+
+#__________________________________________________________________________________________________________________________
+# Function to search each flatlist and return a count of occurences for input
+def search_flatlist(list_to_search, technique_id):
+	count = 0
+	for i in list_to_search:
+		if re.search(technique_id, str(i)):
+			count += 1
+	return count
+
+#__________________________________________________________________________________________________________________________
+# Function to parse the mitre data and extract the Technique ID and Name
+# It then calls the other functions to remove any parent techniques (using technique ID) if a sub technique is present
+# Lastly it exports everything to excel, recreating the Mitre Attack framework
 def parse_json(platform):
 	global count
 	#create empty list for each kill chain phase:
@@ -70,18 +104,29 @@ def parse_json(platform):
 		list_name = []
 		list_of_lists.append(list_name)
 
-
-	#for each kill chain phases
-
 	for lst in platform:
-			for technique in lst:
-				for index in enumerate(deduped_kc):
-					for i in technique["kill_chain_phases"]:
-						for k,v in i.items():
-							if index[1] == v:
-								list_of_lists[index[0]].append(technique["name"].encode("utf-8"))
+		for technique in lst:
+			for i in technique['external_references']:
+				for k,v in i.items():
+					if k == 'external_id' and v.startswith('T'): #extracts the technique ID
+						t_id = v
+			for index in enumerate(deduped_kc):
+				for k,v in technique.items():
+					if k == "kill_chain_phases":
+						for i in v:
+							for k,v in i.items():
+								if index[1] == v:
+									#if len(t_id) < 6: #uncomment this line to remove subtechniques
+										list_of_lists[index[0]].append([t_id, technique["name"]])
 
-	# write to excel file all the data, making first row bold
+	for lst in list_of_lists:
+		flattenedlist = []
+		flattenedlist = flattenNestedList(lst)
+		for i in lst:
+			if search_flatlist(flattenedlist,i[0]) > 1:
+				lst.remove(i)
+
+	# Write to excel file all the data, making first row bold
 	bold_font = Font(bold = True)		#set bold font variable
 	wrap_text = Alignment(wrap_text=True)
 
@@ -99,27 +144,24 @@ def parse_json(platform):
 		cell.font = bold_font
 		cell.alignment = wrap_text
 	
-	#This part is how the Excel columns are built, incrementing character and count
+	# This part is how the Excel columns are built, incrementing character and count
 	column = '@'
 
 	for lst in list_of_lists:
 		column = chr(ord(column) + 1)
 		row = 2
 		for technique in lst:
-			current_sheet[column + str(row)] = technique
-			current_sheet[column + str(row)].alignment = wrap_text
+			current_sheet[column + str(row)] = technique[1]
+			#current_sheet[column + str(row)].alignment = wrap_text
 			row += 1 
 
-	
 	count += 1
-
 
 	wb.save('file_name.xlsx')							
 
-
+#__________________________________________________________________________________________________________________________
+# Call main function with each platform type
 parse_json([all_techniques])
 parse_json([windows]) 
 parse_json([linux])
 parse_json([mac])
-
-
